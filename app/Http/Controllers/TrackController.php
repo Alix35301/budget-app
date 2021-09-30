80f92892667e125aa22ac855916048cdbf0ccf76
ali <ali_x3@live.com>
2021-10-01 02:33:35 +0500

    created tracking table and played with controller class

---
+++ app/Http/Controllers/TrackController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use app\Http\ControllersTrackController;
+use Illuminate\Http\Request;
+
+class TrackController extends Controller
+{
+    
+    public function index(){
+
+    }
+}
